a0bd995f19ee8b5284285f1dfd9d5c5fc5328284
Noam Lior <noamlior2@gmail.com>
2020-01-12 20:58:33 +0200

    add client routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { observer } from 'mobx-react'
 
 import './App.css';
 
 @observer
-class App extends Component{
-  render(){
-    return <div>hello world</div>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+
+        <div id="link-container">
+          <Link to='/transactions'> Transactions</Link>
+          <Link to='/'> Operations</Link>
+        </div>
+
+        <Route path='/transactions' exact render={() =>
+          <Transactions />}>
+        </Route>
+
+        <Route path='/' exact render={() =>
+          <Operations />}>
+        </Route>
+
+      </Router>
+
+    )
   }
 }
 
